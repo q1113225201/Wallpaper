0cd0c299f1e1055c5d7fd1575e0898ecd802c2d2
SJL <1113225201@qq.com>
2017-08-23 22:35:28 +0800

    相机壁纸、视频桌面

---
+++ app/src/main/java/com/sjl/wallpaper/service/CameraWallpaperService.java
@@ -0,0 +1,75 @@
+package com.sjl.wallpaper.service;
+
+import android.hardware.Camera;
+import android.service.wallpaper.WallpaperService;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+
+import java.io.IOException;
+
+/**
+ * 相机壁纸
+ *
+ * @author SJL
+ * @date 2017/8/23
+ */
+public class CameraWallpaperService extends WallpaperService {
+
+    @Override
+    public Engine onCreateEngine() {
+        return new CameraEngine();
+    }
+
+    /**
+     * 相机壁纸的引擎
+     */
+    class CameraEngine extends Engine {
+        Camera camera;
+
+        @Override
+        public void onCreate(SurfaceHolder surfaceHolder) {
+            super.onCreate(surfaceHolder);
+            startPreview();
+            //接受触摸事件
+            setTouchEventsEnabled(true);
+        }
+
+        @Override
+        public void onTouchEvent(MotionEvent event) {
+            super.onTouchEvent(event);
+            //可以在这里处理添加触摸事件，点击拍照之类的
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            stopPreview();
+        }
+
+        /**
+         * 开始预览
+         */
+        private void startPreview() {
+            camera = Camera.open();
+            //需要旋转90度
+            camera.setDisplayOrientation(90);
+            try {
+                camera.setPreviewDisplay(getSurfaceHolder());
+                camera.startPreview();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 关闭预览
+         */
+        private void stopPreview() {
+            if (camera == null) {
+                camera.stopPreview();
+                camera.release();
+                camera = null;
+            }
+        }
+    }
+}
